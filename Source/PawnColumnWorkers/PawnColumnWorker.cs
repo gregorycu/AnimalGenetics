f463d3d57cec502d8979558acb9e3e82c2a270b2
Gregory Currie <gregory.currie@gmail.com>
2020-09-22 21:18:49 +1000

    Fix crash with Numbers mod

---
+++ Source/PawnColumnWorkers/PawnColumnWorker.cs
@@ -0,0 +1,13 @@
+﻿using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace AnimalGenetics
+{
+    class Placeholder : RimWorld.PawnColumnWorker
+    {
+        public override void DoCell(Rect rect, Pawn pawn, PawnTable table)
+        {
+        }
+    }
+}
