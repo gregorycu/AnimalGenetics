4899498fbdaeed0c71aff69a5d4c1d81c84db1d3
Gregory Currie <gregory.currie@gmail.com>
2020-09-26 01:11:17 +1000

    Add DebugAction: Give Birth Together

--- Source/Debug.cs
+++ Source/Debug.cs
@@ -3,9 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Verse;
 
 namespace AnimalGenetics
 {
+    public static class DebugActions
+    {
+        [DebugAction("General", null, allowedGameStates = AllowedGameStates.PlayingOnMap)]
+        private static void GiveBirthTogether()
+        {
+            var males = Find.Selector.SelectedPawns.Where(candidate => candidate.gender == Gender.Male);
+            var females = Find.Selector.SelectedPawns.Where(candidate => candidate.gender == Gender.Female);
+
+            if (males.Count() != 1 || females.Count() != 1)
+                return;
+
+            Hediff_Pregnant.DoBirthSpawn(females.First(), males.First());
+            DebugActionsUtility.DustPuffFrom(females.First());
+        }
+    }
+
     /*
     [HarmonyPatch(typeof(Pawn_AgeTracker), nameof(Pawn_AgeTracker.AgeTick))]
     public class GrowUp
@@ -25,39 +42,39 @@ namespace AnimalGenetics
     }
         */
 
-        /*
-        [HarmonyPatch(typeof(Hediff_Pregnant), nameof(Hediff_Pregnant.Tick))]
-        public class BabyOut
+    /*
+    [HarmonyPatch(typeof(Hediff_Pregnant), nameof(Hediff_Pregnant.Tick))]
+    public class BabyOut
+    {
+        static public bool Prefix(Hediff_Pregnant __instance)
         {
-            static public bool Prefix(Hediff_Pregnant __instance)
-            {
-                __instance.Severity = 1;
-                return true;
-            }
+            __instance.Severity = 1;
+            return true;
         }
-        */
+    }
+    */
 
-        /*
-        [HarmonyPatch(typeof(PawnUtility), nameof(PawnUtility.BodyResourceGrowthSpeed))]
-        public static class FastGrowth
+    /*
+    [HarmonyPatch(typeof(PawnUtility), nameof(PawnUtility.BodyResourceGrowthSpeed))]
+    public static class FastGrowth
+    {
+        static public bool Prefix(ref float __result)
         {
-            static public bool Prefix(ref float __result)
-            {
-                __result = 10000f;
-                return false;
-            }
+            __result = 10000f;
+            return false;
         }
-        */
+    }
+    */
 
-        /*
-        [HarmonyPatch(typeof(CompHatcher), nameof(CompHatcher.CompTick))]
-        public static class FastHatch
+    /*
+    [HarmonyPatch(typeof(CompHatcher), nameof(CompHatcher.CompTick))]
+    public static class FastHatch
+    {
+        static public bool Prefix(ref CompHatcher __instance)
         {
-            static public bool Prefix(ref CompHatcher __instance)
-            {
-                __instance.Hatch();
-                return false;
-            }
+            __instance.Hatch();
+            return false;
         }
-        */
+    }
+    */
 }
