52dfed46f3977a1735dbfcc0fca0f416d9a31f1d
Gregory Currie <gregory.currie@gmail.com>
2020-09-26 15:58:32 +1000

    First iteration of graphs

--- Source/Settings.cs
+++ Source/Settings.cs
@@ -88,28 +88,121 @@ namespace AnimalGenetics
             Scribe_Values.Look<bool>(ref current.showBothParentsInPawnTab, "showBothParentsInPawnTab", DefaultValues.showBothParentsInPawnTab);
         }
 
+        public void DrawGraph(Rect rect, int xMin, int xMax, float mean, float stddev)
+        {
+            if (stddev < 0.5f)
+                stddev = 0.5f;
+
+            float height = rect.height;
+            float width = rect.width;
+
+            float vscale = (height - 30) / Value(mean);
+            float hscale = (width - 40 - 20) / (xMax - xMin);
+
+            GUI.BeginGroup(rect);
+
+            //GUI.color = new Color(0.8f, 0.5f, 0.5f, 1f);
+            //GUI.DrawTexture(new Rect(40, 10, width - 60, height - 20 - 10), TexUI.HighlightTex);
+            GUI.color = Color.grey;
+
+            Widgets.DrawLineHorizontal(40, height - 20, width - 60);
+
+            if (xMin <= 0 && xMax >= 0)
+                Widgets.DrawLineVertical(40 + -xMin * hscale, 10, height - 30);
+
+            if (xMin <= 100 && xMax >= 100)
+                Widgets.DrawLineVertical(40 + (100-xMin) * hscale, 10, height - 30);
+
+            Text.Anchor = TextAnchor.MiddleCenter;
+
+            Widgets.Label(new Rect(0, height - 30, 40, 20), "0%");
+            Widgets.Label(new Rect(20, height - 20, 40, 20), xMin.ToString());
+            Widgets.Label(new Rect(width - 40, height - 20, 40, 20), xMax.ToString());
+
+            float Value(float x)
+            {
+                return (float)
+                    (1f/stddev * System.Math.Sqrt(2f * System.Math.PI)) * (float)System.Math.Pow(System.Math.E, - (x - mean) * (x - mean) / (2f * stddev * stddev));
+            }
+
+            Widgets.Label(new Rect(0, 0, 40, 20), ((int)(100*Value(mean))).ToString()+"%");
+
+            GUI.color = Color.white;
+
+            var prev = new Vector2(xMin, Value(xMin));
+
+            void DrawTo(Vector2 next)
+            {
+                Widgets.DrawLine(
+                    new Vector2(40 + hscale * (prev.x - xMin), -20 + height - vscale * prev.y),
+                    new Vector2(40 + hscale * (next.x - xMin), -20 + height - vscale * next.y),
+                    Color.white, 1);
+                prev = next;
+            }
+
+            for (int offsetX = -15; offsetX <= 15; ++offsetX)
+            {
+                float x = mean + stddev * (offsetX / 5f);
+                if (x <= xMin || x >= xMax)
+                    continue;
+                DrawTo(new Vector2(x, Value(x)));
+            }
+
+            DrawTo(new Vector2(xMax, Value(xMax)));
+
+            GUI.EndGroup();
+
+            Text.Anchor = TextAnchor.UpperLeft;
+        }
+
         public void DoSettingsWindowContents(Rect rect)
         {
             if (!omniscientMode)
                 current.showGenesInWildlifeTab = false;
 
+            float curY = 80;
+
+            Rect generationGraph = new Rect(rect.width / 2 + 10, 0, rect.width / 2 - 10, 0);
+            Rect mutationGraph = new Rect(rect.width / 2 + 10, 0, rect.width / 2 - 10, 0);
+
+            Rect lhs = new Rect(10, curY, rect.width / 2 - 10, 400f);
+            Rect topRhsRect = new Rect(10, curY, rect.width / 2 - 10, 200f);
+
             Listing_Standard listingStandard = new Listing_Standard();
-            listingStandard.Begin(rect);
+            listingStandard.Begin(lhs);
+
+            generationGraph.y = listingStandard.CurHeight;
+
             listingStandard.Label("AG.Settings1".Translate(), -1f, "AG.Settings1Tooltip".Translate());
             listingStandard.Label("AG.Mean".Translate() + " : " + (mean * 100).ToString("F0"));
             mean = listingStandard.Slider(mean, 0f, 2f);
             listingStandard.Label("AG.StandardDeviation".Translate() + " : " + (stdDev * 100).ToString("F0"));
             stdDev = listingStandard.Slider(stdDev, 0f, 0.5f);
-            listingStandard.Gap(30f);
+
+            generationGraph.height = listingStandard.CurHeight - generationGraph.y;
+
+            listingStandard.Gap(20f);
+
+            mutationGraph.y = listingStandard.CurHeight;
+
             listingStandard.Label("AG.Settings2".Translate(), -1f, "AG.Settings2Tooltip".Translate());
             listingStandard.Label("AG.Mean".Translate() + " : " + (mutationMean * 100).ToString("F0"));
             mutationMean = listingStandard.Slider(mutationMean, -0.25f, 0.25f);
             listingStandard.Label("AG.StandardDeviation".Translate() + " : " + (mutationStdDev * 100).ToString("F0"));
             mutationStdDev = listingStandard.Slider(mutationStdDev, 0f, 0.5f);
-            //listingStandard.Gap(30f);
+
+            mutationGraph.height = listingStandard.CurHeight - mutationGraph.y;
+
+            listingStandard.Gap(20f);
             listingStandard.End();
 
-            float curY = listingStandard.CurHeight + 70f;
+            generationGraph.y += curY;
+            mutationGraph.y += curY;
+            DrawGraph(generationGraph, 0, 200, mean * 100, stdDev * 100);
+            DrawGraph(mutationGraph, -25, 25, mutationMean * 100, mutationStdDev * 100);
+
+            curY += listingStandard.CurHeight;
+
             Rect rect2 = new Rect(0, curY, rect.width / 2 - 10, 250f);
             Listing_Standard listingStandard2 = new Listing_Standard();
             listingStandard2.Begin(rect2);
@@ -132,7 +225,7 @@ namespace AnimalGenetics
             listingStandardRhs.End();
 
             Listing_Standard bottom = new Listing_Standard();
-            Rect bottomRect = new Rect(0, rect2.y + listingStandard2.CurHeight + 70f, rect.width, 100);
+            Rect bottomRect = new Rect(0, rect2.y + listingStandard2.CurHeight + 20, rect.width, 100);
             bottom.Begin(bottomRect);
 			if (bottom.ButtonText("AG.DefaultSettings".Translate()))
             {
