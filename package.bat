5b8a53e47f32b077a5851187b1aceddaab61656a
Gregory Currie <gregory.currie@gmail.com>
2021-08-22 20:57:04 +1000

    Add explicit support for 1.3

--- package.bat
+++ package.bat
@@ -1,10 +1,20 @@
 rd /s /q Package
 mkdir Package
+
+IF NOT EXIST ../AnimalGenetics-0.9.zip (
+    powershell -Command Invoke-WebRequest -Uri "https://github.com/gregorycu/AnimalGenetics/releases/download/release%%2F0.9/AnimalGenetics-0.9.zip" -Outfile "../AnimalGenetics-0.9.zip"
+)
+tar -xf ../AnimalGenetics-0.9.zip
+
 mkdir Package\AnimalGenetics
+mkdir Package\AnimalGenetics\1.3
 xcopy /Y /s About Package\AnimalGenetics\About\
-xcopy /Y /s Common Package\AnimalGenetics\Common\
-xcopy /Y /s Defs Package\AnimalGenetics\Defs\
-xcopy /Y /s Languages Package\AnimalGenetics\Languages\
-xcopy /Y /s Patches Package\AnimalGenetics\Patches\
 xcopy /Y LICENSE Package\AnimalGenetics\
-tar -a -c -f Package\AnimalGenetics.zip Package\AnimalGenetics
+xcopy /Y /s Common Package\AnimalGenetics\1.3\
+xcopy /Y /s Defs Package\AnimalGenetics\1.3\Defs\
+xcopy /Y /s Languages Package\AnimalGenetics\1.3\Languages\
+xcopy /Y /s Patches Package\AnimalGenetics\1.3\Patches\
+
+cd Package
+tar -a -c -f AnimalGenetics.zip AnimalGenetics
+cd ..
